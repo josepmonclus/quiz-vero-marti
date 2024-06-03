3047ff23d9c5f38d38170458e124608abf80af54
Josep Monclus <josep.monclus@gmail.com>
2024-06-03 17:59:22 +0200

    Logica de pregunta-resposta

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+export const questions = [
+    {
+        question: 'Número de peu del Josep?',
+        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
+        placeholder: '',
+    },
+    {
+        question: 'Data de naixement de la Gina?',
+        answer: 'c12de9a651f9443cfbed67218d6fde19fb252348837742210ff476e81692a899',
+        placeholder: 'DD/MM/YYYY',
+    }
+]
+
+export const msgs_help = [
+    'Aquí no hem vingut a ajudar-vos! 😁',
+    'Ajuda? 🤣',
+    '🖕',
+    'Mmmhhh... Nop!'
+]
+

c72e52d77d038de366b3bbf6e2f23f61f17faa18
RBernal <me@rubenbernal.com>
2024-06-06 03:02:24 +0200

    Comentarios borrados

--- src/data/data.js
+++ src/data/data.js
@@ -4,86 +4,68 @@ export const questions = [
         question: 'Com es diu el vostre fill?',
         answer: '149aa4c8d80552c78096cc1b1e31b7a72ff36bc09a8f30e859cc0c49b69b4ad8',
         placeholder: '',
-        reward_num:'d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35', //2
-        reward_let:'559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd', //A
+        reward_num:'d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35',
+        reward_let:'559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd',
         reward_type:"num"
     },
     {
         question: 'Número de peu del Josep?',
         answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
         placeholder: '',
-        reward_let:'3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43',//D
-        reward_pos:'4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a',//4
+        reward_let:'3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43',
+        reward_pos:'4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a',
         reward_type:"let"
     },
     {
         question: 'Data de naixement de la Gina?',
         answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
         placeholder: 'DD/MM/YYYY',
-        reward_num:'7902699be42c8a8e46fbbb4501726517e86b22c56a189f7625a6da49081b2451', //7
-        reward_let:'df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c', //B
+        reward_num:'7902699be42c8a8e46fbbb4501726517e86b22c56a189f7625a6da49081b2451',
+        reward_let:'df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c',
         reward_type:"num"
     },
     {
         question: 'Data de naixement de la Gina?',
         answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
         placeholder: 'DD/MM/YYYY',
-        reward_let:'559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd',//A
-        reward_pos:'4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce',//3
+        reward_let:'559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd',
+        reward_pos:'4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce',
         reward_type:"let"
     },
     {
         question: 'Data de naixement de la Gina?',
         answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
         placeholder: 'DD/MM/YYYY',
-        reward_num:'4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a', //4
-        reward_let:'6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d', //C
+        reward_num:'4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a',
+        reward_let:'6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d',
         reward_type:"num"
     },
     {
         question: 'Data de naixement de la Gina?',
         answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
         placeholder: 'DD/MM/YYYY',
-        reward_let:'df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c',//B
-        reward_pos:'6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b',//1
+        reward_let:'df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c',
+        reward_pos:'6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b',
         reward_type:"let"
-    },
-    {
-        question: 'Data de naixement de la Gina?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
-        placeholder: 'DD/MM/YYYY',
-        reward_num:'2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3', //8
-        reward_let:'3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43', //D
-        reward_type:"num"
     },
     {
         question: 'Us ha agradat?',
         answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
         placeholder: 'DD/MM/YYYY',
-        reward_let:'6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d',//C
-        reward_pos:'d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35',//2
+        reward_let:'6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d',
+        reward_pos:'d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35',
         reward_type:"let"
+    },
+    {
+        question: 'Data de naixement de la Gina?',
+        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
+        placeholder: 'DD/MM/YYYY',
+        reward_num:'2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3',
+        reward_let:'3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43',
+        reward_type:"num"
     }
 ]
-//7428
-//BCAD
-/*
-Conversion a Hash - Borrar cuando tengamos la convinacion
-0:5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9 
-1:6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b 
-2:d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35 
-3:4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce 
-4:4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a 
-5:ef2d127de37b942baad06145e54b0c619a1f22327b2ebbcfbec78f5564afe39d 
-6:e7f6c011776e8db7cd330b54174fd76f7d0216b612387a5ffcfb81e6f0919683 
-7:7902699be42c8a8e46fbbb4501726517e86b22c56a189f7625a6da49081b2451 
-8:2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3 
-9:19581e27de7ced00ff1ce50b2047e7a567c76b1cbaebabe5ef03f7c3017bb5b7
-A:559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd
-B:df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c
-C:6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d
-D:3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43
-*/
+
 
 export const msgs_help = [
     'Aquí no hem vingut a ajudar-vos! 😁',

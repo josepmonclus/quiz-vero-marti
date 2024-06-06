b6206083abf2f422152e61c1dd817f71b933e264
Josep Monclus <josep.monclus@gmail.com>
2024-06-06 18:32:53 +0200

    Noves preguntes i canvis d'estils

--- src/data/data.js
+++ src/data/data.js
@@ -9,61 +9,141 @@ export const questions = [
         reward_type:"num"
     },
     {
-        question: 'Número de peu del Josep?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
+        question: 'Data de naixement de l\'Ona?',
+        answer: '56e698f8c7f39694b8284d3698082e0930bd058f0d728ba11ea77a82eaad6bde',
+        placeholder: 'DD/MM/YYYY',
+        reward_let:'',
+        reward_pos:'',
+        reward_type:""
+    },
+    {
+        question: 'Quants minuts duren, en total, totes les pel·lícules de Harry Potter?',
+        answer: 'd40535ac09aac0ef32f8a23b8bb8e04bc2e8f9d6b0981fa98a16d0382f558d83',
         placeholder: '',
         reward_let:'3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43',
         reward_pos:'4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a',
         reward_type:"let"
     },
     {
+        question: 'A què et arrossegar a buscar la Mireia, amb la taja, per barraques?',
+        answer: '9a073c280243877c39078284f07a062e34a58d2b469bbaeb12fedca87d4b4ae2',
+        placeholder: '',
+        reward_let:'',
+        reward_pos:'',
+        reward_type:""
+    },
+    {
         question: 'Data de naixement de la Gina?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
+        answer: 'c12de9a651f9443cfbed67218d6fde19fb252348837742210ff476e81692a899',
         placeholder: 'DD/MM/YYYY',
         reward_num:'7902699be42c8a8e46fbbb4501726517e86b22c56a189f7625a6da49081b2451',
         reward_let:'df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c',
         reward_type:"num"
     },
     {
-        question: 'Data de naixement de la Gina?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
-        placeholder: 'DD/MM/YYYY',
+        question: 'On se li va escapar un pedete a la Vero estant amb la Tania i el Pol?',
+        answer: '4ac7066f88ecd7453d62ad0d610dae4f2cbdd88681fcaceb4cc894f90d3fb13d',
+        placeholder: '',
+        reward_num:'',
+        reward_let:'',
+        reward_type:""
+    },
+    {
+        question: 'A casa de qui vam portar al Bernal tó taja una nit de barraques?',
+        answer: 'c90eb46e709166dac199780b5510be4fa2e356c09eb1cd11fdfd1c44de5c2662',
+        placeholder: '',
         reward_let:'559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd',
         reward_pos:'4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce',
         reward_type:"let"
     },
     {
-        question: 'Data de naixement de la Gina?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
-        placeholder: 'DD/MM/YYYY',
+        question: 'Amb quin programa de 3D ha treballat quasi sempre la Nuria?',
+        answer: '9c8d29d8cee4d84f722cac662ceb72acb53e09766b2f991dd664ced45bb725f1',
+        placeholder: '',
+        reward_let:'',
+        reward_pos:'',
+        reward_type:""
+    },
+    {
+        question: 'Quant sumen les edats, en anys, dels qui us fem aquest fantàstic regal? (a dia del casament)',
+        answer: '580811fa95269f3ecd4f22d176e079d36093573680b6ef66fa341e687a15b5da', 
+        placeholder: '',
         reward_num:'4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a',
         reward_let:'6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d',
         reward_type:"num"
     },
     {
-        question: 'Data de naixement de la Gina?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
-        placeholder: 'DD/MM/YYYY',
+        question: 'A quin grup vam anar a veure tots junts per última vegada?',
+        answer: 'c8bb59164cc453d2245d32ad3aa4ba43b0b22b9289f6646609d9ae3629c74c65', 
+        placeholder: '',
+        reward_num:'',
+        reward_let:'',
+        reward_type:""
+    },
+    {
+        question: 'I a quin restaurant vam anar després a dinar?',
+        answer: 'ea867938d37cf40a755dbc06795157d59caa53901e28fb464fecbde1647ddb09', 
+        placeholder: '',
+        reward_num:'',
+        reward_let:'',
+        reward_type:""
+    },
+    {
+        question: 'Millor lliga de futbol del món?',
+        answer: '6f79fd8e2784166de905800506415b37e094605bcd776178c9e2f0a34147b5a4',
+        placeholder: '',
         reward_let:'df7e70e5021544f4834bbee64a9e3789febc4be81470df629cad6ddb03320a5c',
         reward_pos:'6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b',
         reward_type:"let"
     },
     {
-        question: 'Us ha agradat?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
+        question: 'Quin dia vam carregar el primer pilar de 5?',
+        answer: '19d783ab2a52b90d4827202610b16448d56f143b1ca1e4e42feedd0f13107562',
         placeholder: 'DD/MM/YYYY',
+        reward_let:'',
+        reward_pos:'',
+        reward_type:""
+    },
+    {
+        question: 'Duro como una p____a! LoL!',
+        answer: '09900bf9cb7ea715b4ad842ee06f13ba1f898f4cdd023e280be5656b574537ef',
+        placeholder: '',
+        reward_let:'',
+        reward_pos:'',
+        reward_type:""
+    },
+    {
+        question: 'Quin sobrenom li va posar a la Gina certa persona que no anomenarem?',
+        answer: '852d8667884cc2e47ece1cfc03399c3f75d725baabf9c92416bf3cf028b2b1ec',
+        placeholder: '',
         reward_let:'6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d',
         reward_pos:'d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35',
         reward_type:"let"
     },
     {
-        question: 'Data de naixement de la Gina?',
-        answer: '31489056e0916d59fe3add79e63f095af3ffb81604691f21cad442a85c7be617',
+        question: 'Data de naixement de l\'Aleix?',
+        answer: '339aa7a9f82bfd482109df77a5435b7850e159a03b0e4c88980369effc741eb7',
+        placeholder: 'DD/MM/YYYY',
+        reward_num:'',
+        reward_let:'',
+        reward_type:""
+    },
+    {
+        question: 'Quin dia va anar Red Helmet a Hoy No Se Sale?',
+        answer: 'e151ed61ff56f836656fcee7160e9ae5ed467e9aa34b9ae656fd3151baee7209', 
         placeholder: 'DD/MM/YYYY',
         reward_num:'2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3',
         reward_let:'3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43',
         reward_type:"num"
-    }
+    },
+    {
+        question: 'Doncs ja ho tindriem, no ha sigut per tant no? Quina és la combinació?',
+        answer: '7a48e12bda814dd8ef4a0e7d95bc728388224b45fc80e6286985228161426659',
+        placeholder: '',
+        reward_num:'',
+        reward_let:'',
+        reward_type:""
+    },
 ]
 
 
